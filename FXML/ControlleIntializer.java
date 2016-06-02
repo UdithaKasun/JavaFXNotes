0b80fd6e6a9ce3515b9c81b30f82ea8d1796925a
Uditha Kasun Senavirathna <udithakasun311@gmail.com>
2016-06-02 14:04:57 +0530

    Create ControlleIntializer.java

---
+++ FXML/ControlleIntializer.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller implements Initializable{
+	  
+	  //To get initialize method you must implement Initializable Interface
+	  
+		//These are reference to the Controllers mentioned FXML file
+		@FXML
+		private Button btnSubmit;
+		@FXML
+		private TextField txtName;
+		@FXML
+		private Label lblText;
+		
+		
+		// This action is Performed when btnSubmit actionPerformed()
+		public void getOutput(){
+			System.out.println("Input : "+txtName.getText());
+			lblText.setText(txtName.getText());
+		}
+
+    //This method runs after form loading is complete
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			System.out.println("Loading User Data");
+		}
+	
+}

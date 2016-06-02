87390eb7ae4882f4d52dbae7c58f8163c5d529d6
Uditha Kasun Senavirathna <udithakasun311@gmail.com>
2016-06-02 22:53:54 +0530

    Create Introduction.java

---
+++ Properties-Event-Binding/Collections/Introduction.java
@@ -0,0 +1,19 @@
+Collections in JavaFX are defined by the javafx.collections package, which consists of the following interfaces and classes:
+
+(1) Interfaces
+
+ObservableList: A list that enables listeners to track changes when they occur
+
+ListChangeListener: An interface that receives notifications of changes to an ObservableList
+
+ObservableMap: A map that enables observers to track changes when they occur
+
+MapChangeListener: An interface that receives notifications of changes to an ObservableMap
+
+(2) Classes
+
+FXCollections: A utility class that consists of static methods that are one-to-one copies of java.util.Collections methods
+
+ListChangeListener.Change: Represents a change made to an ObservableList
+
+MapChangeListener.Change: Represents a change made to an ObservableMap

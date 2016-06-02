43bfbbdf24eeec8a9ed191efbb71d6ea19ce9978
Uditha Kasun Senavirathna <udithakasun311@gmail.com>
2016-06-02 22:02:53 +0530

    Update and rename Notes.java to NoteBinding(1).java

--- Properties-Event-Binding/Binding/NoteBinding(1).java
+++ Properties-Event-Binding/Binding/NoteBinding(1).java
@@ -51,7 +51,37 @@ public class Main {
 }
 
 *** Running this example will print the message "Electric bill has changed" to standard output, proving that the change listener 		 		notification is working.
- 
+
+*** addListener method is availble in all JavaFX Properties.
+
+ someProperty.addListener(new ChangeListener<Object>() {
+			@Override
+			public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+				// TODO Auto-generated method stub
+			}
+ );
+
+
+<-- ChangeListener -->
+
+(1) A ChangeListener can be attached to any ObservableValue (e.g. JavaFX Properties) and it gets notified, if the value of the           ObservableValue changes. 
+
+(2) An obvious advantage is, that old and new value are provided automatically in the method handler. But this advantage is rather       minor and can be neglected.  
+
+(3) A more important advantage is, that a ChangeListener only gets fired, if the value REALLY changes. (InvalidationListener do not guarantee this, as we will see in a minute.) 
+
+(4) The disadvantage of a ChangeListener is, that it enforces eager calculation of bindings. Because we want to notify a ChangeListener only, if the value really changed, we have to calculate the new result of a binding immediately.
+
+<-- InvalidationListener -->
+
+(1) InvalidationListener can be attached to any Observable (e.g. any ObservableValue is an Observable). 
+
+(2) It is notified when the Observable may have changed – there is no guarantee, that it really changed. 
+
+(3) The main advantage is, that the runtime does not have to compare old value and new value. 
+
+(4) In other words, bindings can still be calculated lazily, if an InvalidationListener is attached. The disadvantage is, that it may fire if the value did not really change.
+
 
 
 

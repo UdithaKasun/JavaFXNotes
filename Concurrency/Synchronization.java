132955e21c776ac2c5bfdb2e469f10bd88291697
Uditha Kasun Senavirathna <udithakasun311@gmail.com>
2016-06-05 23:11:22 +0530

    Update Synchronization.java

--- Concurrency/Synchronization.java
+++ Concurrency/Synchronization.java
@@ -1,1 +1,28 @@
+[ Synchronized Methods ]
+
+A synchronized method includes the synchronized keyword in its header. 
+
+(1) Instance Methods
+
+  public class ID{
+  
+    int counter = 0;
+  
+    public synchronized int getID(){
+      return counter++;
+    }
+  
+  }
+
+Lock is associated with the object on which method is called.
+
+  ID id = new ID();
+  System.out.println(id.getID());
+
+The lock is associated with the ID object whose reference is stored in id. 
+
+If another thread called id.getID() while this method was executing, the other thread would have
+to wait until the executing thread released the lock.
+
+
 
